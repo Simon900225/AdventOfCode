1bb0dc80ddb1764fff99b66ea5dbdac979e07df5
Simon900225 <simon900225@gmail.com>
2022-12-20 21:36:37 +0100

    Advent 6

--- Advent6/Program.cs
+++ Advent6/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more informationt
-Console.WriteLine("Hello, World!");
+﻿var input = new StreamReader("input.txt").ReadToEnd();
+
+Console.WriteLine($"Start of packet: {FindUniqueSequence(input, 4)}");
+Console.WriteLine($"Start of message: {FindUniqueSequence(input, 14)}");
+
+static int FindUniqueSequence(string input, int length)
+{
+    var queue = new Queue<char>();
+    int i = 0;
+
+    for (; i < input.Length; i++)
+    {
+        queue.Enqueue(input[i]);
+
+        if (queue.Count > length)
+        {
+            queue.Dequeue();
+            if (queue.Distinct().Count() == length)
+                break;
+        }
+    }
+
+    return i + 1;
+}
